df140215fe08e8a8a65b4953ee1e316db0af988b
AustinPotts <pottsevolvements@gmail.com>
2019-08-26 14:02:16 -0400

    Collection Views Storyboard

---
+++ HairCare/HairCare/View Controllers/MyProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MyProfileViewController.swift
+//  HairCare
+//
+//  Created by Austin Potts on 8/26/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import UIKit
+
+class MyProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HairCare/HairCare/Views/StylistCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  StylistCollectionViewCell.swift
+//  HairCare
+//
+//  Created by Austin Potts on 8/26/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import UIKit
+
+class StylistCollectionViewCell: UICollectionViewCell {
+    
+}
