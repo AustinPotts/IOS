7c06419afb52c02ff6536f45b71c56270ce4cfe0
AustinPotts <pottsevolvements@gmail.com>
2019-08-28 17:04:31 -0400

    Stylist Profile Image Update/ Select Stylist VC Change

--- HairCare/HairCare/Model Controllers/StylistController.swift
+++ HairCare/HairCare/Model Controllers/StylistController.swift
@@ -12,8 +12,23 @@ class StylistController {
     
     var stylist: [HairStylist] = [
         HairStylist(name: "Lady1", yearsOfExperience: 3, imageName: "lady1"),
-        HairStylist(name: "Lady2", yearsOfExperience: 2, imageName: "lady2"),
-        HairStylist(name: "Lady3", yearsOfExperience: 5, imageName: "lady3"),
+        HairStylist(name: "Lady3", yearsOfExperience: 2, imageName: "lady3"),
+        HairStylist(name: "Lady4", yearsOfExperience: 5, imageName: "lady4"),
+        HairStylist(name: "Lady6", yearsOfExperience: 3, imageName: "lady6"),
+        HairStylist(name: "Lady7", yearsOfExperience: 2, imageName: "lady7"),
+        HairStylist(name: "Lady5", yearsOfExperience: 5, imageName: "lady5"),
+        HairStylist(name: "Lady9", yearsOfExperience: 2, imageName: "lady9"),
+        HairStylist(name: "Lady10", yearsOfExperience: 5, imageName: "lady10"),
+        HairStylist(name: "Lady11", yearsOfExperience: 3, imageName: "lady11"),
+        HairStylist(name: "Lady12", yearsOfExperience: 2, imageName: "lady12"),
+        HairStylist(name: "Lady13", yearsOfExperience: 5, imageName: "lady13"),
+        HairStylist(name: "Lady14", yearsOfExperience: 5, imageName: "lady14"),
+        HairStylist(name: "Lady15", yearsOfExperience: 3, imageName: "lady15"),
+        HairStylist(name: "Lady16", yearsOfExperience: 2, imageName: "lady16"),
+        HairStylist(name: "Lady17", yearsOfExperience: 5, imageName: "lady17"),
+        HairStylist(name: "Lady18", yearsOfExperience: 3, imageName: "lady18"),
+        HairStylist(name: "Lady19", yearsOfExperience: 2, imageName: "lady19"),
+        HairStylist(name: "Lady20", yearsOfExperience: 5, imageName: "lady20"),
     ]
     
 }
---
+++ HairCare/HairCare/View Controllers/SelectStylistViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SelectStylistViewController.swift
+//  HairCare
+//
+//  Created by Austin Potts on 8/28/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import UIKit
+
+class SelectStylistViewController: UIViewController {
+    
+    @IBOutlet weak var selectStylistImageView: UIImageView!
+    var stylist: HairStylist?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       //updateViews()
+    }
+    
+//    func updateViews() {
+//        if let selStylist = stylist {
+//            image = selStylist.image
+//        }
+//    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
